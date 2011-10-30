0aa721fc06c19372c0c1bfdfb921980a3d7b5ce2
mmx900 <mmx900@gmail.com>
2011-10-30 15:44:43 +0900

    샘플 플러그인 추가

---
+++ ManalithBot/src/main/java/org/manalith/ircbot/plugin/sample/SamplePlugin.java
@@ -0,0 +1,57 @@
+package org.manalith.ircbot.plugin.sample;
+
+import org.jibble.pircbot.User;
+import org.manalith.ircbot.BotMain;
+import org.manalith.ircbot.plugin.IBotPlugin;
+import org.manalith.ircbot.resources.MessageEvent;
+
+public class SamplePlugin implements IBotPlugin {
+
+	public String getName() {
+		return "Sample Plugin";
+	}
+
+	public String getNamespace() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getHelp() {
+		return "!친반묘";
+	}
+
+	public void onJoin(String channel, String sender, String login,
+			String hostname) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void onMessage(MessageEvent event) {
+		String message = event.getMessage();
+		String channel = event.getChannel();
+		
+		if(message.equals("!친반묘")){
+			User[] users = BotMain.BOT.getUsers(channel);
+			boolean isMyo = false;
+			for(User u : users){
+				if(u.getNick().equals("myojok")){
+					isMyo = true;
+					break;
+				}
+			}
+			BotMain.BOT.sendLoggedMessage(channel, isMyo ? "(두리번) ... 친묘!" : "(두리번) +_+/ 멸묘!");
+			event.setExecuted(true);
+		}
+	}
+
+	public void onPart() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void onQuit() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
