4b87b7cecbe1041c6e7012db9e7c4b51aa901bce
root <root@darkcircle.(none)>
2011-10-30 21:26:41 +0900

    Calc modules is added

---
+++ ManalithBot/src/main/java/org/manalith/ircbot/plugin/Calc/TokenArray.java
@@ -0,0 +1,62 @@
+package org.manalith.ircbot.plugin.Calc;
+// TokenArray.java 
+//
+// This class can store many TokenUnit elements.
+//
+// This program can be distributed under the terms of GNU GPL v2 or later.
+// darkcircle.0426@gmail.com
+
+import java.util.ArrayList;
+public class TokenArray {
+	private ArrayList<TokenUnit> TokenArray;
+	
+	public TokenArray ( )
+	{
+		TokenArray = new ArrayList<TokenUnit>();
+	}
+	public TokenArray ( TokenArray sourceObject )
+	{
+		TokenArray = new ArrayList<TokenUnit>();
+		TokenArray.addAll(sourceObject.getArray());
+	}
+	public TokenArray ( ArrayList<TokenUnit> sourceArray )
+	{
+		TokenArray = new ArrayList<TokenUnit>();
+		TokenArray.addAll(sourceArray);
+	}
+	
+	public void addToken ( TokenType newTokenType, TokenSubtype newTokenSubtype, String newTokenStr )
+	{
+		TokenUnit newTokenUnit = new TokenUnit ( newTokenType, newTokenSubtype, newTokenStr );
+		TokenArray.add( newTokenUnit );
+	}
+	public void addToken ( TokenUnit newTokenUnit )
+	{
+		TokenArray.add( newTokenUnit );
+	}
+	
+	public TokenUnit getToken ( int index )
+	{
+		return TokenArray.get( index );
+	}
+	public int getSize ( )
+	{
+		return TokenArray.size();
+	}
+	public ArrayList<TokenUnit> getArray ( )
+	{
+		return this.TokenArray;
+	}
+	
+	public String toString ( )
+	{
+		String result = "";
+		
+		for ( TokenUnit unit : TokenArray )
+		{
+			result += unit.toString() + "\n";
+		}
+		
+		return result;
+	}
+}
