78f61cb3e1eaee19e661d05ab618bc611189547d
mmx900 <mmx900@gmail.com>
2012-01-02 00:34:01 +0900

    구글 검색 API를 사용하여 검색하도록 수정

--- ManalithBot/src/main/java/org/manalith/ircbot/plugin/google/GooglePlugin.java
+++ ManalithBot/src/main/java/org/manalith/ircbot/plugin/google/GooglePlugin.java
@@ -9,13 +9,9 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 import org.manalith.ircbot.ConfigurationManager;
 import org.manalith.ircbot.ManalithBot;
 import org.manalith.ircbot.plugin.AbstractBotPlugin;
@@ -108,49 +104,46 @@ public class GooglePlugin extends AbstractBotPlugin {
 	}
 
 	public String getGoogleTopResult(String keyword) {
-		// google.co.kr은 검색 결과가 다르다. (최상단의 핫링크에 대한 처리 필요)
-		final String url = "http://www.google.com/search?hl=ko";
-
 		try {
-			// google.co.kr은 User-Agent가 Mozilla 계열로 감지되어야만 UTF-8 문서를 반환하며 다른
-			// 헤더들은 무시된다.
-			Document doc = Jsoup
-					.connect(url)
-					.userAgent(
-							"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:8.0) Gecko/20100101 Firefox/8.0")
-					.header("Accept-Charset", "utf-8")
-					.header("Accept-Language", "ko-kr, ko;q=0.8")
-					.header("Content-type", "text/html; charset=UTF-8")
-					.data("q", keyword).get();
+			// http://code.google.com/apis/websearch/docs/#fonje
+			URL url = new URL(
+					"https://ajax.googleapis.com/ajax/services/search/web?v=1.0&"
+							+ "q="
+							+ URLEncoder.encode(keyword, "UTF-8")
+							+ "&key="
+							+ ConfigurationManager
+									.getInstance()
+									.get("org.manalith.ircbot.bot.plugin.google.key")
+							+ "&userip="
+							+ InetAddress.getLocalHost().getHostAddress());
+			URLConnection connection = url.openConnection();
+			connection.addRequestProperty("Referer", "http://manalith.org");
 
-			Elements topResult = doc.select("#rso>li:eq(0)");
-
-			if (!topResult.isEmpty()) {
-				Elements titleElements = topResult.select("h3:eq(0)>a");
-				String title = titleElements.text();
-				String resultUrl = titleElements.attr("href");
-
-				String summary = topResult.select("span.st").text();
-
-				String result = "[" + title + "] " + summary + " - "
-						+ resultUrl;
-
-				// HTML 코드 처리
-				// result = result.replace("<em>", "[").replace("</em>", "]")
-				// .replace("<b>", "").replace("</b>", "")
-				// .replace("<br />", "").replace("&quot;", "\"")
-				// .replace("&amp;", "&").replace("&middot;", "-");
-
-				// 길이 점검
-				result = StringUtils.substring(result, 0,
-						bot.getMaxLineLength() / 3);
-
-				return result;
-			} else {
-				return String.format("[%s] 검색 결과가 없습니다.", keyword);
+			String line;
+			StringBuilder builder = new StringBuilder();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					connection.getInputStream()));
+			while ((line = reader.readLine()) != null) {
+				builder.append(line);
 			}
 
+			JSONObject firstResult = new JSONObject(builder.toString())
+					.getJSONObject("responseData").getJSONArray("results")
+					.getJSONObject(0);
+			String result = firstResult.getString("content") + " : "
+					+ firstResult.getString("url");
+
+			// HTML 코드 처리
+			result = result.replace("<b>", "[").replace("</b>", "]")
+					.replace("&quot;", "\"").replace("&amp;", "&");
+
+			return result;
+
+		} catch (MalformedURLException e) {
+			logger.error(e);
 		} catch (IOException e) {
+			logger.error(e);
+		} catch (JSONException e) {
 			logger.error(e);
 		}
 
