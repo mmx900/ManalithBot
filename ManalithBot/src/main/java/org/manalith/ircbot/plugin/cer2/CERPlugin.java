0e141513a71e55da5f0bff7f3ffb8ba8404df744
Darkcircle <darkcircle.0426@gmail.com>
2012-01-24 17:35:46 +0900

    Latest CERPlugin.java is merged into darkcircle's branch

--- ManalithBot/src/main/java/org/manalith/ircbot/plugin/cer2/CERPlugin.java
+++ ManalithBot/src/main/java/org/manalith/ircbot/plugin/cer2/CERPlugin.java
@@ -1,20 +1,21 @@
 /*
- 	org.manalith.ircbot.plugin.cer2/CERPlugin.java
- 	ManalithBot - An open source IRC bot based on the PircBot Framework.
- 	Copyright (C) 2011  Seong-ho, Cho <darkcircle.0426@gmail.com>
+	org.manalith.ircbot.plugin.cer2/CERPlugin.java
+	ManalithBot - An open source IRC bot based on the PircBot Framework.
+	Copyright (C) 2011, 2012 Seong-ho, Cho <darkcircle.0426@gmail.com>
+	Copyright (C) 2012  Changwoo Ryu <cwryu@debian.org>
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.manalith.ircbot.plugin.cer2;
 
@@ -44,7 +45,7 @@ public class CERPlugin extends AbstractBotPlugin {
 	 * @see org.manalith.ircbot.plugin.IBotPlugin#getNamespace()
 	 */
 	public String getNamespace() {
-		return "환율";
+		return "curex|환율";
 	}
 
 	/*
@@ -83,79 +84,68 @@ public class CERPlugin extends AbstractBotPlugin {
 		String channel = event.getChannel();
 
 		String[] command = msg.split("\\s");
-		if (command[0].length() < 3)
-		{
-			// do not recognize as cmd and just return
+		if (!command[0].equals("!curex") && !command[0].equals("!환율")
+				&& !command[0].startsWith("!curex:")
+				&& !command[0].startsWith("!환율:"))
 			return;
-		}
-		else if (command[0].substring(0, 3).equals("!환율")) {
-			String[] subcmd = command[0].split("\\:");
-			if (!subcmd[0].equals("!환율"))
-				return;
-			else {
-				if (subcmd.length == 1) {
-					String mergedcmd = "";
-					for (int i = 1; i < command.length; i++) {
-						mergedcmd += command[i];
-						if (i != command.length - 1)
-							mergedcmd += " ";
-					}
 
-					try {
-						CERRunner runner = new CERRunner(
-								event.getSender(), this.getResourcePath(),
-								mergedcmd);
-
-						String result = runner.run();
-						if (result.equals("Help!")) {
-							/*
-							bot.sendLoggedMessage(channel,
-									CERInfoProvider.getIRCHelpMessagePart1());
-							bot.sendLoggedMessage(channel,
-									CERInfoProvider.getIRCHelpMessagePart2());
-							bot.sendLoggedMessage(channel,
-									CERInfoProvider.getIRCHelpMessagePart3());
-							bot.sendLoggedMessage(channel,
-									CERInfoProvider.getIRCHelpMessagePart4());
-							*/
-							bot.sendLoggedMessage(channel, "도움말 그런거 없다능! (츤츤)ㄴ");
-						} else {
-							bot.sendLoggedMessage(channel, result);
-						}
-					} catch (Exception e) {
-						bot.sendLoggedMessage(channel, e.getMessage());
-					}
-				}
-				else if (subcmd.length > 2)
-				{
-					bot.sendLoggedMessage(channel, "옵션이 너무 많습니다");
-				}
-				else {
-					// remerge strings separated by space.
-					String userNick = event.getSender();
-
-					String arg = "";
-					for (int i = 1; i < command.length; i++) {
-						if (command[i].equals(" "))
-							continue;
-						arg += command[i];
-					}
-
-					CERCustomSettingManager csMan = new CERCustomSettingManager(
-							this.getResourcePath(), channel, userNick, arg);
-
-					if (subcmd[1].equals("sub"))
-						bot.sendLoggedMessage(channel, csMan.addUserSetting());
-					else if (subcmd[1].equals("unsub"))
-						
-						bot.sendLoggedMessage(channel,
-								csMan.removeUserSetting());
-					else
-						bot.sendLoggedMessage(channel, "그런 옵션은 없습니다.");
-
-				}
-				event.setExecuted(true);
+		String[] subcmd = command[0].split("\\:");
+		if (subcmd.length == 1) {
+			String mergedcmd = "";
+			for (int i = 1; i < command.length; i++) {
+				mergedcmd += command[i];
+				if (i != command.length - 1)
+					mergedcmd += " ";
 			}
+
+			try {
+				CERRunner runner = new CERRunner(event.getSender(),
+						this.getResourcePath(), mergedcmd);
+
+				String result = runner.run();
+				if (result.equals("Help!")) {
+					/*
+					bot.sendLoggedMessage(channel,
+							CERInfoProvider.getIRCHelpMessagePart1());
+					bot.sendLoggedMessage(channel,
+							CERInfoProvider.getIRCHelpMessagePart2());
+					bot.sendLoggedMessage(channel,
+							CERInfoProvider.getIRCHelpMessagePart3());
+					bot.sendLoggedMessage(channel,
+							CERInfoProvider.getIRCHelpMessagePart4());
+					*/
+					bot.sendLoggedMessage(channel, "도움말 그런거 없음!");
+				} else {
+					bot.sendLoggedMessage(channel, result);
+				}
+			} catch (Exception e) {
+				bot.sendLoggedMessage(channel, e.getMessage());
+			}
+		} else if (subcmd.length > 2) {
+			bot.sendLoggedMessage(channel, "옵션이 너무 많습니다");
+		} else {
+			// remerge strings separated by space.
+			String userNick = event.getSender();
+
+			String arg = "";
+			for (int i = 1; i < command.length; i++) {
+				if (command[i].equals(" "))
+					continue;
+				arg += command[i];
+			}
+
+			CERCustomSettingManager csMan = new CERCustomSettingManager(
+					this.getResourcePath(), channel, userNick, arg);
+
+			if (subcmd[1].equals("sub"))
+				bot.sendLoggedMessage(channel, csMan.addUserSetting());
+			else if (subcmd[1].equals("unsub"))
+
+				bot.sendLoggedMessage(channel, csMan.removeUserSetting());
+			else
+				bot.sendLoggedMessage(channel, "그런 옵션은 없습니다.");
+
 		}
+		event.setExecuted(true);
 	}
 }
