ea0a9202fa34ea9ba1d1120221f07cbfd2a21d86
Darkcircle <darkcircle.0426@gmail.com>
2012-01-24 17:59:31 +0900

    Commands are partial changed to Korean.

--- ManalithBot/src/main/java/org/manalith/ircbot/plugin/calc/CalcPlugin.java
+++ ManalithBot/src/main/java/org/manalith/ircbot/plugin/calc/CalcPlugin.java
@@ -33,11 +33,11 @@ public class CalcPlugin extends AbstractBotPlugin {
 	}
 
 	public String getCommands() {
-		return "eval";
+		return "계산";
 	}
 
 	public String getHelp() {
-		return "!eval (계산식), sin(), cos(), tan(), arcsin(), arccos(), arctan(), tobin(정수계산식), tooct(정수계산식), todec(정수계산식), tohex(정수계산식)";
+		return "!계산 (계산식), sin(), cos(), tan(), arcsin(), arccos(), arctan(), tobin(정수계산식), tooct(정수계산식), todec(정수계산식), tohex(정수계산식)";
 	}
 
 	public void onMessage(MessageEvent event) {
@@ -45,7 +45,7 @@ public class CalcPlugin extends AbstractBotPlugin {
 		String channel = event.getChannel();
 		String[] command = message.split("\\s");
 
-		if (command[0].equals("!eval")) {
+		if (command[0].equals("!계산")) {
 			if (command.length == 1) {
 				bot.sendLoggedMessage(channel, "입력한 식이 없습니다.");
 				bot.sendLoggedMessage(channel, this.getHelp());
