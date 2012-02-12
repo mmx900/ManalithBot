2db6df5ca6e6727f3ebdc708f029399f8365272d
mmx900 <mmx900@gmail.com>
2012-02-13 00:58:11 +0900

    빌드 및 실행 스크립트 추가

---
+++ ManalithBot/build.bat
@@ -0,0 +1,2 @@
+REM mvn install:install-file -Dfile=lib/sqlitejdbc-v056.jar -DgroupId=com.zentus -DartifactId=sqlitejdbc -Dversion=v056 -Dpackaging=jar
+mvn package
--- ManalithBot/build.sh
+++ ManalithBot/build.sh
@@ -1,1 +1,2 @@
-mvn install:install-file -Dfile=lib/sqlitejdbc-v056.jar -DgroupId=com.zentus -DartifactId=sqlitejdbc -Dversion=v056 -Dpackaging=jar
+#mvn install:install-file -Dfile=lib/sqlitejdbc-v056.jar -DgroupId=com.zentus -DartifactId=sqlitejdbc -Dversion=v056 -Dpackaging=jar
+mvn package
