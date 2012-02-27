54eb39ff38b379a059761e3adb99e0ba80d68a2c
mmx900 <mmx900@gmail.com>
2012-02-27 22:52:44 +0900

    사용되지 않는 클래스 제거

--- ManalithBot/src/main/java/org/manalith/ircbot/plugin/dictionary/AlreadyRegisteredException.java
+++
@@ -1,7 +0,0 @@
-package org.manalith.ircbot.plugin.dictionary;
-
-public class AlreadyRegisteredException extends Exception {
-
-	private static final long serialVersionUID = 519934487338070951L;
-
-}
