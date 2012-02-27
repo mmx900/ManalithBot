e86fc992ae360926452613b2c054a9ce1b54cdf9
mmx900 <mmx900@gmail.com>
2012-02-27 22:24:03 +0900

    사전 저장시 기존 글을 삭제하지 않도록 수정

--- ManalithBot/src/main/java/org/manalith/ircbot/plugin/dictionary/AlreadyRegisteredException.java
+++ ManalithBot/src/main/java/org/manalith/ircbot/plugin/dictionary/AlreadyRegisteredException.java
@@ -2,4 +2,6 @@ package org.manalith.ircbot.plugin.dictionary;
 
 public class AlreadyRegisteredException extends Exception {
 
+	private static final long serialVersionUID = 519934487338070951L;
+
 }
--- ManalithBot/src/main/java/org/manalith/ircbot/plugin/dictionary/NotRegisteredException.java
+++ ManalithBot/src/main/java/org/manalith/ircbot/plugin/dictionary/NotRegisteredException.java
@@ -2,4 +2,6 @@ package org.manalith.ircbot.plugin.dictionary;
 
 public class NotRegisteredException extends Exception {
 
+	private static final long serialVersionUID = -3952858865987240733L;
+
 }
