074382624092fc1e50aafe9274bcacea86a5d08f
mmx900 <mmx900@gmail.com>
2012-02-28 18:40:15 +0900

    어노테이션 기반 플러그인 작성 지원 - 누락된 파일 추가

---
+++ ManalithBot/src/main/java/org/manalith/ircbot/plugin/sample/PlainOldBotPlugin.java
@@ -0,0 +1,55 @@
+/*
+ 	org.manalith.ircbot.plugin.sample/SamplePlugin.java
+ 	ManalithBot - An open source IRC bot based on the PircBot Framework.
+ 	Copyright (C) 2011, 2012  Ki-Beom, Kim
+ 	Copyright (C) 2012 Seong-ho, Cho <darkcircle.0426@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.manalith.ircbot.plugin.sample;
+
+import org.manalith.ircbot.plugin.AbstractBotPlugin;
+import org.manalith.ircbot.resources.MessageEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PlainOldBotPlugin extends AbstractBotPlugin {
+
+	public String getName() {
+		return "Sample Plugin";
+	}
+
+	public String getCommands() {
+		return "!hello";
+	}
+
+	public String getHelp() {
+		return "!hello";
+	}
+
+	public void onMessage(MessageEvent event) {
+		this.onMessage(event, event.getChannel());
+	}
+
+	protected void onMessage(MessageEvent event, String target) {
+		String message = event.getMessage();
+		String channel = event.getChannel();
+
+		if (message.equals("!hello")) {
+			bot.sendLoggedMessage(channel, "Hello World!");
+			
+			event.setExecuted(true);
+		}
+	}
+}
