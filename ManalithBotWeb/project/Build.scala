1121d3cda25d605e90f855d0c78cc8bfb29300cf
mmx900 <mmx900@gmail.com>
2012-05-06 22:16:41 +0900

    사전 플러그인 웹 연동

--- ManalithBotWeb/project/Build.scala
+++ ManalithBotWeb/project/Build.scala
@@ -4,15 +4,16 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "ManalithBotWeb"
-    val appVersion      = "1.0-SNAPSHOT"
+	val appName = "ManalithBotWeb"
+	val appVersion = "1.0-SNAPSHOT"
 
-    val appDependencies = Seq(
-      // Add your project dependencies here,
-    )
+	val appDependencies = Seq(
+		// Add your project dependencies here,
+		"org.hibernate" % "hibernate-entitymanager" % "3.6.10.Final",
+		"org.hsqldb" % "hsqldb" % "2.2.8")
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      // Add your own project settings here      
-    )
+	val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
+		// Add your own project settings here
+		ebeanEnabled := false)
 
 }
