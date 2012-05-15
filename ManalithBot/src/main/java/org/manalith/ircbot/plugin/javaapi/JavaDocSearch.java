a3926def3503098458d3fc643a1dc6b69bacbbb3
mmx900 <mmx900@gmail.com>
2012-05-16 06:10:43 +0900

    JavaApi 플러그인에 JSoup 적용

--- ManalithBot/src/main/java/org/manalith/ircbot/plugin/javaapi/JavaDocSearch.java
+++ ManalithBot/src/main/java/org/manalith/ircbot/plugin/javaapi/JavaDocSearch.java
@@ -4,26 +4,20 @@
 package org.manalith.ircbot.plugin.javaapi;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.HttpConnectionParams;
 import org.apache.log4j.Logger;
-import org.w3c.dom.Document;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.NodeList;
-import org.w3c.tidy.Tidy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 
 public class JavaDocSearch implements SearchInterface{
 	private Logger logger = Logger.getLogger(getClass());
-	private final String JAVADOC_BASE_URL;
+	private final String baseUrl;
 	private static final String  JAVADOC_ALLCLASS_FRAME_URL = "allclasses-frame.html";
 	private List<JavaDocObject> docObjects = null;
 	
@@ -32,63 +26,41 @@ public class JavaDocSearch implements SearchInterface{
 	 * @param baseUrl JavaDoc의 기본 URL (예 : http://java.sun.com/j2se/1.5.0/docs/api/)
 	 */
 	public JavaDocSearch(String baseUrl){
-		JAVADOC_BASE_URL = baseUrl;
+		this.baseUrl = baseUrl;
 	}
 	
 	private Document getDocument(){
-		HttpClient client = new DefaultHttpClient();
-		HttpGet method = null;
-		InputStream inputStream = null;
-		Document doc = null;
-		
-		try{
-			method = new HttpGet(JAVADOC_BASE_URL + JAVADOC_ALLCLASS_FRAME_URL);
-			HttpConnectionParams.setConnectionTimeout(client.getParams(), 8000);
-
-			HttpResponse response= client.execute(method);
-			//client.executeMethod(method);
-			//inputStream = method.getResponseBodyAsStream();
-			
-			Tidy tidy = new Tidy();
-			tidy.setXHTML(true);
-			doc = tidy.parseDOM(inputStream, null);
-//		}catch (HttpException e) {
-//			logger.error(e);
+		try {
+			return Jsoup.connect(baseUrl + JAVADOC_ALLCLASS_FRAME_URL).get();
 		} catch (IOException e) {
-			logger.error(e);
-		} finally {
-			client.getConnectionManager().shutdown();
+			e.printStackTrace();
 		}
 		
-		
-		return doc;
+		return null;
 	}
 	
 	private List<JavaDocObject> parseDocObjects(Document doc){
-		NodeList items = doc.getElementsByTagName("a");
+		Elements items = doc.select("a");
+		
 		List<JavaDocObject> objs = new ArrayList<JavaDocObject>();
 		
-		for(int i=0; i < items.getLength(); i++){
+		for(int i=0; i < items.size(); i++){
+			Element element = items.get(i);
 			JavaDocObject o = new JavaDocObject();
 			
 			//A 태그 안에 클래스 이름이 있을 경우
-			String nodeValue = items.item(i).getChildNodes().item(0).getNodeValue();
+			String nodeValue = element.text();
 			if(StringUtils.isEmpty(nodeValue)){
 				//A 태그 안의 I 태그 안에 인터페이스 이름이 있을 경우
-				o.setName(items.item(i).getChildNodes().item(0).getChildNodes().item(0).getNodeValue());
+				o.setName(element.select("i").get(0).text());
 				o.setType(JavaDocObject.INTERFACE);
 			}else{
 				o.setName(nodeValue);
 				o.setType(JavaDocObject.CLASS);
 			}
 			
-			NamedNodeMap nnm = items.item(i).getAttributes();
-			for(int y=0; y < nnm.getLength(); y++){
-				if(nnm.item(y).getNodeName().equals("href")){
-					o.setURL(nnm.item(y).getNodeValue());
-					break;
-				}
-			}
+			o.setURL(element.attr("href"));
+			
 			objs.add(o);
 		}
 		
@@ -97,6 +69,7 @@ public class JavaDocSearch implements SearchInterface{
 	
 	/**
 	 * 검색하여 일치하는 1개의 클래스를 가져 온다.
+	 * 
 	 * @param className 검색할 클래스의 이름
 	 * @return 클래스 이름 + 클래스 api 링크
 	 */
@@ -112,7 +85,7 @@ public class JavaDocSearch implements SearchInterface{
 				sb.append(o.getName());
 				sb.append("]");
 				sb.append(" - ");
-				sb.append(JAVADOC_BASE_URL);
+				sb.append(baseUrl);
 				sb.append(o.getURL());
 				
 				break;
@@ -124,6 +97,7 @@ public class JavaDocSearch implements SearchInterface{
 	
 	/**
 	 * 검색하여 해당 패턴에 해당하는 클래스명을 가진 모든 클래스를 가져온다.
+	 * 
 	 * @param regex 검색할 클래스의 이름(정규표현식)
 	 * @param classes 검색 결과를 추가할 리스트, null일 경우 새로이 생성하여 리턴한다.
 	 * @return 리스트(클래스 이름 + 클래스 api 링크)
@@ -141,7 +115,7 @@ public class JavaDocSearch implements SearchInterface{
 				sb.append(o.getName());
 				sb.append("]");
 				sb.append(" - ");
-				sb.append(JAVADOC_BASE_URL);
+				sb.append(baseUrl);
 				sb.append(o.getURL());
 				classes.add(sb.toString());
 			}
