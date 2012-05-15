57211dc48af57a97332d1868f7d81a235daa2b98
mmx900 <mmx900@gmail.com>
2012-05-16 06:09:24 +0900

    오래된 API 주소 갱신

--- ManalithBot/src/main/java/org/manalith/ircbot/plugin/javaapi/JavaDocSources.java
+++ ManalithBot/src/main/java/org/manalith/ircbot/plugin/javaapi/JavaDocSources.java
@@ -13,18 +13,20 @@ public class JavaDocSources {
 	//TODO : 시작시 properties 파일에서 소스 목록을 가져오게끔 변경할 것
 	static{
 		sources.put("hibernate","http://www.hibernate.org/hib_docs/v3/api/");
-		sources.put("tomcat","http://jakarta.apache.org/tomcat/tomcat-5.5-doc/servletapi/");
-		sources.put("servlet","http://jakarta.apache.org/tomcat/tomcat-5.5-doc/servletapi/");
-		sources.put("jsp","http://jakarta.apache.org/tomcat/tomcat-5.5-doc/jspapi/");
+		sources.put("tomcat","http://tomcat.apache.org/tomcat-5.5-doc/servletapi/");
+		sources.put("servlet","http://tomcat.apache.org/tomcat-5.5-doc/servletapi/");
+		sources.put("jsp","http://tomcat.apache.org/tomcat-5.5-doc/jspapi/");
 		sources.put("tidy","http://jtidy.sourceforge.net/apidocs/");
-		sources.put("j2se","http://java.sun.com/j2se/1.5.0/docs/api/");
-		sources.put("1.5","http://java.sun.com/j2se/1.5.0/docs/api/");
-		sources.put("1.5.0","http://java.sun.com/j2se/1.5.0/docs/api/");
-		sources.put("5.0","http://java.sun.com/j2se/1.5.0/docs/api/");
-		sources.put("1.4","http://java.sun.com/j2se/1.4.2/docs/api/");
-		sources.put("1.4.2","http://java.sun.com/j2se/1.4.2/docs/api/");
-		sources.put("1.3","http://java.sun.com/j2se/1.3.1/docs/api/");
-		sources.put("1.3.1","http://java.sun.com/j2se/1.3.1/docs/api/");
+		sources.put("j2se","http://docs.oracle.com/javase/7/docs/api/");
+		sources.put("7","http://docs.oracle.com/javase/7/docs/api/");
+		sources.put("6","http://docs.oracle.com/javase/6/docs/api/");
+		sources.put("1.5","http://docs.oracle.com/javase/1.5.0/docs/api/");
+		sources.put("1.5.0","http://docs.oracle.com/javase/1.5.0/docs/api/");
+		sources.put("5.0","http://docs.oracle.com/javase/1.5.0/docs/api/");
+		sources.put("1.4","http://docs.oracle.com/javase/1.4.2/docs/api/");
+		sources.put("1.4.2","http://docs.oracle.com/javase/1.4.2/docs/api/");
+		sources.put("1.3","http://docs.oracle.com/javase/1.3.1/docs/api/");
+		sources.put("1.3.1","http://docs.oracle.com/javase/1.3.1/docs/api/");
 	}
 	
 	private JavaDocSources(){};
