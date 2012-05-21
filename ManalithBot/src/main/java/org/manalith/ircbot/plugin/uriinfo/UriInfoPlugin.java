1b3cb81da9efef1617159993fc68dd358a291f5f
Seong-ho, Cho <darkcircle.0426@gmail.com>
2012-05-22 03:59:28 +0900

    urititle is renamed to uriinfo with bringing up content-type printing feature

--- ManalithBot/src/main/java/org/manalith/ircbot/plugin/uriinfo/UriInfoPlugin.java
+++ ManalithBot/src/main/java/org/manalith/ircbot/plugin/uriinfo/UriInfoPlugin.java
@@ -1,7 +1,8 @@
 /*
- 	org.manalith.ircbot.plugin.urititle/UriTitlePlugin.java
+ 	org.manalith.ircbot.plugin.uriinfo/UriInfoPlugin.java
  	ManalithBot - An open source IRC bot based on the PircBot Framework.
  	Copyright (C) 2012  Changwoo Ryu <cwryu@debian.org>
+ 	Copyright (C) 2012  Seong-ho Cho <darkcircle.0426@gmail.com>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -16,11 +17,19 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.manalith.ircbot.plugin.urititle;
+package org.manalith.ircbot.plugin.uriinfo;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIUtils;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.manalith.ircbot.plugin.AbstractBotPlugin;
@@ -28,7 +37,7 @@ import org.manalith.ircbot.resources.MessageEvent;
 import org.springframework.stereotype.Component;
 
 @Component
-public class UriTitlePlugin extends AbstractBotPlugin {
+public class UriInfoPlugin extends AbstractBotPlugin {
 	private Logger logger = Logger.getLogger(getClass());
 
 	public String getCommands() {
@@ -57,7 +66,61 @@ public class UriTitlePlugin extends AbstractBotPlugin {
 		return matcher.group(1);
 	}
 
-	private String getTitle(String uri) {
+	private String getInfo(String newUri) {
+		
+		String result;
+		
+		// split scheme from uri
+		String scheme = newUri.split("\\:\\/\\/")[0];
+		String uriWOScheme = newUri.split("\\:\\/\\/")[1];
+		
+		// split query from uri
+		String query = "";
+		if ( uriWOScheme.split("\\?").length >= 2 )
+			 query = uriWOScheme.split("\\?")[1];
+		
+		// split hostname and path
+		String [] hp = uriWOScheme.split("\\?")[0].split("\\/");
+		String host = hp[0];
+		String path = "";
+		if ( hp.length > 1 )
+			for ( int i = 1; i < hp.length; i++ )
+			{
+				if ( i != 1 ) path += "/";
+				path += hp[i];
+			}
+			
+		try {
+			HttpResponse resp = (new DefaultHttpClient()).execute(new HttpGet(
+					URIUtils.createURI(scheme, host, -1, path, query, null)));
+			String content_type = (new URL(newUri)).openConnection()
+					.getContentType();
+			
+			// all possible failure case
+			if (resp.getStatusLine().getStatusCode() != 200
+					|| !content_type.contains("text/")
+					|| !content_type.contains("application/")
+					&& !content_type.contains("ml")) {
+				result = "[Link Content-type] "
+						+ (new URL(newUri)).openConnection().getContentType();
+			} 
+			else 
+			{
+				result = "[Link Title] "
+						+ Jsoup.connect(newUri)
+								.header("User-Agent",
+										"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:12.0) Gecko/20100101 Firefox/12.0")
+								.get().title().replaceAll("\\n", "")
+								.replaceAll("\\r", "")
+								.replaceAll("(\\s){2,}", " ");
+			}
+		} 
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			result = "";
+		}
+		/*
 		try {
 			return Jsoup
 					.connect(uri)
@@ -69,6 +132,9 @@ public class UriTitlePlugin extends AbstractBotPlugin {
 			logger.warn(e.getMessage(), e);
 			return null;
 		}
+		//*/
+		
+		return result;
 	}
 
 	public void onMessage(MessageEvent event) {
@@ -79,9 +145,9 @@ public class UriTitlePlugin extends AbstractBotPlugin {
 		if (uri == null)
 			return;
 
-		String title = getTitle(uri);
-		if (title != null) {
-			bot.sendLoggedMessage(channel, "[Link Title] " + title);
+		String info = getInfo(uri);
+		if (info != null) {
+			bot.sendLoggedMessage(channel, info);
 		}
 
 		// This plugin runs implicitly; NO need to call
