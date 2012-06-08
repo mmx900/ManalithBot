a0edd101656ea728d35c5e397c83e02212f62ce8
mmx900 <mmx900@gmail.com>
2012-06-08 23:24:05 +0900

    역변환이 가능하도록 기능 개선

--- ManalithBot/src/main/java/org/manalith/ircbot/plugin/symbol/UnicodePlugin.java
+++ ManalithBot/src/main/java/org/manalith/ircbot/plugin/symbol/UnicodePlugin.java
@@ -24,16 +24,31 @@ public class UnicodePlugin extends AbstractBotPlugin {
 		if (segments.length == 2
 				&& (segments[0].equals("!유니코드") || segments[0]
 						.equals("!unicode"))) {
-			StringBuilder sb = new StringBuilder();
-			char[] chars = segments[1].toCharArray();
-			for (Character ch : chars) {
-				sb.append("U+" + Integer.toHexString(ch | 0x10000).substring(1));
+			String text = segments[1];
+			try {
+				if (text.startsWith("\\u") || text.startsWith("U+")) {
+					char ch = (char) Integer.parseInt(text.substring(2), 16);
+					event.respond(ch + " - " + Character.getName(ch));
+				} else if (text.startsWith("U+")) {
+					char ch = (char) Integer.parseInt(text.substring(2), 16);
+					event.respond(ch + " - " + Character.getName(ch));
+				} else {
+					char[] chars = text.toCharArray();
+					StringBuilder sb = new StringBuilder();
+					for (char ch : chars) {
+						sb.append("U+"
+								+ Integer.toHexString(ch | 0x10000)
+										.substring(1));
+					}
+					if (chars.length == 1) {
+						sb.append(" - ");
+						sb.append(Character.getName(chars[0]));
+					}
+					event.respond(sb.toString());
+				}
+			} catch (NumberFormatException e) {
+				event.respond("잘못된 유니코드 문자열입니다.");
 			}
-			if (chars.length == 1) {
-				sb.append(" - ");
-				sb.append(Character.getName(chars[0]));
-			}
-			event.respond(sb.toString());
 		}
 	}
 
