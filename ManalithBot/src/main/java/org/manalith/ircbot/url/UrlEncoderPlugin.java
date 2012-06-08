467bc1e6b48c14a4ca25acd1dd81e6f9f854bc0a
mmx900 <mmx900@gmail.com>
2012-06-08 23:44:57 +0900

    URL 인코더 / 디코더 플러그인 추가

---
+++ ManalithBot/src/main/java/org/manalith/ircbot/url/UrlEncoderPlugin.java
@@ -0,0 +1,47 @@
+package org.manalith.ircbot.url;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
+import org.manalith.ircbot.plugin.AbstractBotPlugin;
+import org.manalith.ircbot.resources.MessageEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UrlEncoderPlugin extends AbstractBotPlugin {
+	private Logger logger = Logger.getLogger(getClass());
+
+	@Override
+	public String getName() {
+		return "URL 변환 플러그인";
+	}
+
+	@Override
+	public String getCommands() {
+		return "!urlencode [url] 혹은 !urldecode [url]";
+	}
+
+	@Override
+	public void onMessage(MessageEvent event) {
+		String[] segments = StringUtils.split(event.getMessage(), " ");
+		if (segments.length == 2) {
+			String cmd = segments[0];
+			String txt = segments[1];
+
+			try {
+				if (cmd.equals("!urlencode")) {
+					event.respond(URLEncoder.encode(txt, "UTF-8"));
+				} else if (cmd.equals("!urldecode")) {
+					event.respond(URLDecoder.decode(txt, "UTF-8"));
+				}
+			} catch (UnsupportedEncodingException e) {
+				// impossible
+				logger.error(e);
+			}
+		}
+	}
+
+}
