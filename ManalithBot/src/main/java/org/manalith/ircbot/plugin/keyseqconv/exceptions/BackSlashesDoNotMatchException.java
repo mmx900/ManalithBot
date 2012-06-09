179fa9f9244901313c554d7bffe423ee71ee4bcf
Seong-ho, Cho <darkcircle.0426@gmail.com>
2012-06-10 01:10:06 +0900

    typeconv is renamed to keyseqconv and added feature for Sebeol-layout

---
+++ ManalithBot/src/main/java/org/manalith/ircbot/plugin/keyseqconv/exceptions/BackSlashesDoNotMatchException.java
@@ -0,0 +1,11 @@
+package org.manalith.ircbot.plugin.keyseqconv.exceptions;
+
+public class BackSlashesDoNotMatchException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public BackSlashesDoNotMatchException ( )
+	{
+		super ( "Back slashes do not match");
+	}
+}
