a80aab7c6be04509ddce8b1fa51fd733b41027d4
Seong-ho, Cho <darkcircle.0426@gmail.com>
2012-06-10 01:05:58 +0900

    modify relay plugin to use ignorePattern and outputFormat from RelayPluginConfiguration

--- ManalithBot/src/main/java/org/manalith/ircbot/plugin/relay/RelayPlugin.java
+++ ManalithBot/src/main/java/org/manalith/ircbot/plugin/relay/RelayPlugin.java
@@ -17,7 +17,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class RelayPlugin extends AbstractBotPlugin {
 	public static RelayBot RELAY_BOT;
-
+	private String outputFormat;
+	
 	public RelayPlugin(boolean enableRelay,
 			RelayPluginConfigurationManager config) throws Exception {
 		// TODO enableRelay 인자를 받지 않고도 플러그인 초기화를 막을 수 있도록 구조 개선
@@ -30,9 +31,14 @@ public class RelayPlugin extends AbstractBotPlugin {
 		final RelayBot bot = new RelayBot(config.getBotLogin(),
 				config.getBotName());
 		RELAY_BOT = bot;
+		// XXX Refer RelayBotListener.java:7
+		bot.setIgnorePattern(config.getIgnorePattern());
+		bot.setOutputFormat(config.getOutputFormat());
 		bot.setVerbose(config.getVerbose());
 		bot.setEncoding(config.getServerEncoding());
 		bot.connect(config.getServer(), config.getServerPort());
+		
+		this.outputFormat = config.getOutputFormat();
 
 		final StringTokenizer st = new StringTokenizer(
 				config.getDefaultChannels(), ",");
@@ -52,27 +58,40 @@ public class RelayPlugin extends AbstractBotPlugin {
 		return null;
 	}
 
-	@Override
 	public void setBot(ManalithBot bot) {
 		RELAY_BOT.setTargetbot(bot);
-		super.setBot(bot);
 	}
 
 	public void onMessage(MessageEvent event) {
+		
 		String channel = event.getChannel().getName();
+		String sender = event.getUser().getNick();
+		String message = event.getMessage();
 
-		if (event.getMessage().equals("relay:stop")) {
+		if (message.equals("relay:stop")) {
 			setRelaying(false);
 			event.respond("릴레이를 강제로 종료합니다.");
-		} else if (event.getMessage().equals("relay:start")) {
+			// 타겟 채널 이름은 소스 채널 이름과 동일한 것으로 간주.
+			// 명령에 대한 안내에 대해 입력 명령은 전송하지 않음.
+			RELAY_BOT.sendMessage(event.getChannel().getName(),"릴레이를 강제로 종료합니다.");
+			event.setExecuted(true);
+			return;
+		} else if (message.equals("relay:start")) {
 			setRelaying(true);
 			event.respond("릴레이를 강제로 시작합니다.");
+			// 타겟 채널 이름은 소스 채널 이름과 동일한 것으로 간주.
+			// 명령에 대한 안내에 대해 입력 명령은 전송하지 않음.
+			RELAY_BOT.sendMessage(event.getChannel().getName(),"릴레이를 강제로 시작합니다.");
+			event.setExecuted(true);
+			return;
 		}
 
 		if (isRelaying()) {
 			/**
 			 * 메인 봇에 메시지가 들어오면 릴레이 봇으로 전송한다.
 			 */
+			
+			/*
 			String targetChannel = "#setzer";
 
 			if (channel.equals("#gnome")) {
@@ -80,9 +99,11 @@ public class RelayPlugin extends AbstractBotPlugin {
 			} else if (channel.equals("#setzer")) {
 				targetChannel = "#setzer";
 			}
-
-			RELAY_BOT.sendMessage(targetChannel, "<"
-					+ event.getUser().getNick() + "> " + event.getMessage());
+			*/
+			
+			// 타겟 서버의 타겟 채널 이름은 소스 서버의 소스 채널 이름과 동일한 것으로 간주.
+			RELAY_BOT.sendMessage(channel, 
+				String.format(this.outputFormat, sender, message) );
 		}
 	}
 
