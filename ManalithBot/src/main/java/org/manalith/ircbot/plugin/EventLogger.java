68f16a5487b67911b9a559373c28e3850cf110d6
mmx900 <mmx900@gmail.com>
2012-12-04 16:28:02 +0900

    [#89] IRC 이벤트 로깅 레벨 변경

--- ManalithBot/src/main/java/org/manalith/ircbot/plugin/EventLogger.java
+++ ManalithBot/src/main/java/org/manalith/ircbot/plugin/EventLogger.java
@@ -23,19 +23,19 @@ public class EventLogger extends ListenerAdapter<ManalithBot> {
 
 	@Override
 	public void onConnect(ConnectEvent<ManalithBot> event) throws Exception {
-		logger.trace("CONNECT");
+		logger.info("CONNECT");
 	}
 
 	@Override
 	public void onDisconnect(DisconnectEvent<ManalithBot> event)
 			throws Exception {
-		logger.trace("DISCONNECT");
+		logger.info("DISCONNECT");
 	}
 
 	@Override
 	public void onServerResponse(ServerResponseEvent<ManalithBot> event)
 			throws Exception {
-		logger.trace(String.format("SERVER_RESPONSE: %s / %s", event.getCode(),
+		logger.info(String.format("SERVER_RESPONSE: %s / %s", event.getCode(),
 				event.getResponse()));
 	}
 
@@ -47,21 +47,21 @@ public class EventLogger extends ListenerAdapter<ManalithBot> {
 		String hostname = event.getUser().getHostmask();
 		String message = event.getMessage();
 
-		logger.trace(String.format("MESSAGE : %s / %s / %s / %s / %s", channel,
+		logger.info(String.format("MESSAGE : %s / %s / %s / %s / %s", channel,
 				sender, login, hostname, message));
 	}
 
 	@Override
 	public void onPrivateMessage(PrivateMessageEvent<ManalithBot> event)
 			throws Exception {
-		logger.trace(String.format("PRIVMSG : %s / %s / %s / %s", event
+		logger.info(String.format("PRIVMSG : %s / %s / %s / %s", event
 				.getUser().getNick(), event.getUser().getLogin(), event
 				.getUser().getHostmask(), event.getMessage()));
 	}
 
 	@Override
 	public void onAction(ActionEvent<ManalithBot> event) throws Exception {
-		logger.trace(String.format("ACTION : %s / %s / %s / %s / %s", event
+		logger.info(String.format("ACTION : %s / %s / %s / %s / %s", event
 				.getChannel().getName(), event.getUser().getNick(), event
 				.getUser().getLogin(), event.getUser().getHostmask(), event
 				.getAction()));
@@ -69,14 +69,14 @@ public class EventLogger extends ListenerAdapter<ManalithBot> {
 
 	@Override
 	public void onJoin(JoinEvent<ManalithBot> event) throws Exception {
-		logger.trace(String.format("JOIN : %s / %s / %s / %s", event
+		logger.info(String.format("JOIN : %s / %s / %s / %s", event
 				.getChannel().getName(), event.getUser().getNick(), event
 				.getUser().getLogin(), event.getUser().getHostmask()));
 	}
 
 	@Override
 	public void onPart(PartEvent<ManalithBot> event) throws Exception {
-		logger.trace(String.format("PART : %s / %s / %s / %s", event
+		logger.info(String.format("PART : %s / %s / %s / %s", event
 				.getChannel().getName(), event.getUser().getNick(), event
 				.getUser().getLogin(), event.getUser().getHostmask()));
 	}
@@ -84,14 +84,14 @@ public class EventLogger extends ListenerAdapter<ManalithBot> {
 	@Override
 	public void onNickChange(NickChangeEvent<ManalithBot> event)
 			throws Exception {
-		logger.trace(String.format("NICK_CHANGE : %s / %s / %s / %s", event
+		logger.info(String.format("NICK_CHANGE : %s / %s / %s / %s", event
 				.getOldNick(), event.getUser().getLogin(), event.getUser()
 				.getHostmask(), event.getNewNick()));
 	}
 
 	@Override
 	public void onKick(KickEvent<ManalithBot> event) throws Exception {
-		logger.trace(String.format("KICK : %s / %s / %s / %s / %s / %s", event
+		logger.info(String.format("KICK : %s / %s / %s / %s / %s / %s", event
 				.getChannel().getName(), event.getSource().getNick(), event
 				.getSource().getLogin(), event.getSource().getHostmask(), event
 				.getRecipient().getNick(), event.getReason()));
@@ -99,7 +99,7 @@ public class EventLogger extends ListenerAdapter<ManalithBot> {
 
 	@Override
 	public void onQuit(QuitEvent<ManalithBot> event) throws Exception {
-		logger.trace(String.format("QUIT : %s / %s / %s / %s", event.getUser()
+		logger.info(String.format("QUIT : %s / %s / %s / %s", event.getUser()
 				.getNick(), event.getUser().getLogin(), event.getUser()
 				.getHostmask(), event.getReason()));
 
@@ -107,7 +107,7 @@ public class EventLogger extends ListenerAdapter<ManalithBot> {
 
 	@Override
 	public void onTopic(TopicEvent<ManalithBot> event) throws Exception {
-		logger.trace(String.format("TOPIC : %s / %s / %s / %s / %s", event
+		logger.info(String.format("TOPIC : %s / %s / %s / %s / %s", event
 				.getChannel().getName(), event.getTopic(), event.getChannel()
 				.getTopicSetter(), event.getTimestamp(), event.isChanged()));
 	}
