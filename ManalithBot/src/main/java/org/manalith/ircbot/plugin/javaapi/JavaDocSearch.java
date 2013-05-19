aecc8e1ffb26719894233fa3e14edbe3970b6321
Seong-ho Cho <darkcircle.0426@gmail.com>
2013-05-19 21:46:42 +0900

    eliminate warning

--- ManalithBot/src/main/java/org/manalith/ircbot/plugin/javaapi/JavaDocSearch.java
+++ ManalithBot/src/main/java/org/manalith/ircbot/plugin/javaapi/JavaDocSearch.java
@@ -8,108 +8,111 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-
-public class JavaDocSearch implements SearchInterface{
-	private Logger logger = Logger.getLogger(getClass());
+public class JavaDocSearch implements SearchInterface {
+	// private Logger logger = Logger.getLogger(getClass());
 	private final String baseUrl;
-	private static final String  JAVADOC_ALLCLASS_FRAME_URL = "allclasses-frame.html";
+	private static final String JAVADOC_ALLCLASS_FRAME_URL = "allclasses-frame.html";
 	private List<JavaDocObject> docObjects = null;
-	
+
 	/**
 	 * 
-	 * @param baseUrl JavaDoc의 기본 URL (예 : http://java.sun.com/j2se/1.5.0/docs/api/)
+	 * @param baseUrl
+	 *            JavaDoc의 기본 URL (예 : http://java.sun.com/j2se/1.5.0/docs/api/)
 	 */
-	public JavaDocSearch(String baseUrl){
+	public JavaDocSearch(String baseUrl) {
 		this.baseUrl = baseUrl;
 	}
-	
-	private Document getDocument(){
+
+	private Document getDocument() {
 		try {
 			return Jsoup.connect(baseUrl + JAVADOC_ALLCLASS_FRAME_URL).get();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		return null;
 	}
-	
-	private List<JavaDocObject> parseDocObjects(Document doc){
+
+	private List<JavaDocObject> parseDocObjects(Document doc) {
 		Elements items = doc.select("a");
-		
+
 		List<JavaDocObject> objs = new ArrayList<JavaDocObject>();
-		
-		for(int i=0; i < items.size(); i++){
+
+		for (int i = 0; i < items.size(); i++) {
 			Element element = items.get(i);
 			JavaDocObject o = new JavaDocObject();
-			
-			//A 태그 안에 클래스 이름이 있을 경우
+
+			// A 태그 안에 클래스 이름이 있을 경우
 			String nodeValue = element.text();
-			if(StringUtils.isEmpty(nodeValue)){
-				//A 태그 안의 I 태그 안에 인터페이스 이름이 있을 경우
+			if (StringUtils.isEmpty(nodeValue)) {
+				// A 태그 안의 I 태그 안에 인터페이스 이름이 있을 경우
 				o.setName(element.select("i").get(0).text());
 				o.setType(JavaDocObject.INTERFACE);
-			}else{
+			} else {
 				o.setName(nodeValue);
 				o.setType(JavaDocObject.CLASS);
 			}
-			
+
 			o.setURL(element.attr("href"));
-			
+
 			objs.add(o);
 		}
-		
+
 		return objs;
 	}
-	
+
 	/**
 	 * 검색하여 일치하는 1개의 클래스를 가져 온다.
 	 * 
-	 * @param className 검색할 클래스의 이름
+	 * @param className
+	 *            검색할 클래스의 이름
 	 * @return 클래스 이름 + 클래스 api 링크
 	 */
-	public String search(String className){
-		if(docObjects == null)
+	public String search(String className) {
+		if (docObjects == null)
 			docObjects = parseDocObjects(getDocument());
-		
+
 		StringBuffer sb = new StringBuffer();
-		
-		for(JavaDocObject o : docObjects){
-			if(o.getName().equalsIgnoreCase(className)){
+
+		for (JavaDocObject o : docObjects) {
+			if (o.getName().equalsIgnoreCase(className)) {
 				sb.append("[");
 				sb.append(o.getName());
 				sb.append("]");
 				sb.append(" - ");
 				sb.append(baseUrl);
 				sb.append(o.getURL());
-				
+
 				break;
 			}
 		}
-		
+
 		return sb.toString();
 	}
-	
+
 	/**
 	 * 검색하여 해당 패턴에 해당하는 클래스명을 가진 모든 클래스를 가져온다.
 	 * 
-	 * @param regex 검색할 클래스의 이름(정규표현식)
-	 * @param classes 검색 결과를 추가할 리스트, null일 경우 새로이 생성하여 리턴한다.
+	 * @param regex
+	 *            검색할 클래스의 이름(정규표현식)
+	 * @param classes
+	 *            검색 결과를 추가할 리스트, null일 경우 새로이 생성하여 리턴한다.
 	 * @return 리스트(클래스 이름 + 클래스 api 링크)
 	 */
-	public List<String> searchAll(String regex, List<String> classes){
-		if(classes == null) classes = new ArrayList<String>();
-		
-		if(docObjects == null)
+	public List<String> searchAll(String regex, List<String> classes) {
+		if (classes == null)
+			classes = new ArrayList<String>();
+
+		if (docObjects == null)
 			docObjects = parseDocObjects(getDocument());
-		
-		for(JavaDocObject o : docObjects){
-			if(o.getName().matches(regex)){
+
+		for (JavaDocObject o : docObjects) {
+			if (o.getName().matches(regex)) {
 				StringBuffer sb = new StringBuffer();
 				sb.append("[");
 				sb.append(o.getName());
@@ -120,7 +123,7 @@ public class JavaDocSearch implements SearchInterface{
 				classes.add(sb.toString());
 			}
 		}
-		
+
 		return classes;
 	}
 }
