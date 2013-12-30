93c3e45c89e9d6d03376265e4b5ec481ddcb00f8
mmx900 <mmx900@gmail.com>
2013-12-31 01:45:50 +0900

    코드 클린업

--- ManalithBot/src/main/java/org/manalith/ircbot/plugin/admin/DebugPlugin.java
+++ ManalithBot/src/main/java/org/manalith/ircbot/plugin/admin/DebugPlugin.java
@@ -2,10 +2,10 @@ package org.manalith.ircbot.plugin.admin;
 
 import java.nio.charset.Charset;
 
-import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.manalith.ircbot.annotation.NotNull;
+import org.manalith.ircbot.common.stereotype.BotCommand;
 import org.manalith.ircbot.plugin.SimplePlugin;
-import org.manalith.ircbot.resources.MessageEvent;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -22,17 +22,13 @@ public class DebugPlugin extends SimplePlugin {
 		return StringUtils.join(COMMANDS, ",");
 	}
 
-	@Override
-	public void onMessage(MessageEvent event) {
-		String[] segs = event.getMessageSegments();
-		if (segs.length > 1 && ArrayUtils.contains(COMMANDS, segs[0])) {
-			String values = StringUtils.substringAfter(event.getMessage(), " ");
-			char first = StringUtils.left(values, 1).charAt(0);
-			char last = StringUtils.right(values, 1).charAt(0);
-			event.respond(String.format(
-					"%d char / %d bytes ('%s' %s ~ '%s' %s)", values.length(),
-					values.getBytes(Charset.forName("UTF-8")).length, first,
-					Character.getName(first), last, Character.getName(last)));
-		}
+	@BotCommand
+	public String len(@NotNull String values) {
+		char first = StringUtils.left(values, 1).charAt(0);
+		char last = StringUtils.right(values, 1).charAt(0);
+		return String.format("%d char / %d bytes ('%s' %s ~ '%s' %s)",
+				values.length(),
+				values.getBytes(Charset.forName("UTF-8")).length, first,
+				Character.getName(first), last, Character.getName(last));
 	}
 }
