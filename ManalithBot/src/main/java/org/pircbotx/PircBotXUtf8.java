1283f7c9f245e16f0a7838870c5df55adbef74c2
mmx900 <mmx900@gmail.com>
2014-01-11 23:24:30 +0900

    [#130] PircBotX 2.0 업데이트 이후 긴 문자열 깨짐

--- ManalithBot/src/main/java/org/pircbotx/PircBotXUtf8.java
+++ ManalithBot/src/main/java/org/pircbotx/PircBotXUtf8.java
@@ -1,97 +1,19 @@
 package org.pircbotx;
 
+import org.pircbotx.output.OutputRaw;
+import org.pircbotx.output.OutputRawUTF8;
+
 public class PircBotXUtf8 extends PircBotX {
+	private final OutputRawUTF8 outputRaw;
+
 	public PircBotXUtf8(Configuration<? extends PircBotX> configuration) {
 		super(configuration);
+
+		outputRaw = new OutputRawUTF8(this);
 	}
 
-	public void sendRawLineSplit(String prefix, String message, String suffix) {
-		// Make sure suffix is valid
-		if (message == null)
-			return;
-
-		if (configuration.getEncoding().toString() != "UTF-8") {
-			super.sendRaw().rawLineSplit(prefix, message, suffix);
-			return;
-		}
-
-		if (suffix == null)
-			suffix = "";
-
-		// Make a server side prefix string
-		User b = getUserBot();
-		String serverSidePrefix = ":" + b.getNick() + "!" + b.getLogin() + "@"
-				+ b.getHostmask() + " ";
-
-		byte[] serverSidePrefixByteArray = serverSidePrefix.getBytes();
-		byte[] prefixByteArray = prefix.getBytes();
-		byte[] messageByteArray = message.getBytes();
-		byte[] suffixByteArray = suffix.getBytes();
-		byte[] messageByteArrayCopy;
-
-		// Find if final line is going to be shorter than the max line length
-		String finalMessage = prefix + message + suffix;
-
-		int realMaxLineLength = configuration.getMaxLineLength() - 2; // except
-																		// CR+LF
-		if (!configuration.autoSplitMessage
-				|| serverSidePrefixByteArray.length + prefixByteArray.length
-						+ messageByteArray.length + suffixByteArray.length < realMaxLineLength) {
-			// Length is good (or auto split message is false),
-			// just go ahead and send it
-			sendRaw().rawLine(finalMessage);
-			return;
-		}
-
-		// Too long, split it up ( with considering server side prefix length )
-		int maxMessageLength = realMaxLineLength
-				- (serverSidePrefixByteArray.length + prefixByteArray.length + suffixByteArray.length);
-
-		int startPoint = 0;
-		int endPoint = -1;
-		byte val;
-
-		while (true) {
-			endPoint = ((startPoint + maxMessageLength < messageByteArray.length) ? (startPoint + maxMessageLength)
-					: messageByteArray.length) - 1;
-
-			// some characters ( such as cjk character ) need to separate
-			// correctly.
-			while (true) {
-
-				if (endPoint == messageByteArray.length - 1)
-					break;
-
-				val = (byte) (messageByteArray[endPoint] & 0xC0);
-
-				if (val == (byte) 0x80) {
-					// upper ASCII area
-					endPoint--;
-				} else if (val == (byte) 0x40 || val == 0x0) {
-					// within ASCII area
-					break;
-				} else {
-					// highest byte of UTF-8 code
-					endPoint--;
-					break;
-				}
-			}
-
-			// extract some part of byte array
-			messageByteArrayCopy = new byte[endPoint - startPoint + 1];
-			System.arraycopy(messageByteArray, startPoint,
-					messageByteArrayCopy, 0, endPoint - startPoint + 1);
-
-			// convert from byte array to string and concatenate!
-			sendRaw().rawLine(
-					prefix + new String(messageByteArrayCopy) + suffix);
-
-			startPoint = endPoint + 1;
-
-			// if endPoint reached to last index of messageByteArray,
-			// stop to separate raw line.
-			if (startPoint == messageByteArray.length)
-				break;
-		}
+	@Override
+	public OutputRaw sendRaw() {
+		return outputRaw;
 	}
 }
