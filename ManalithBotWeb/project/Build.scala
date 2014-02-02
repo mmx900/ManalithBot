d0beed491af1fbd33c3091707f588f2ca0ebddb1
mmx900 <mmx900@gmail.com>
2014-02-02 18:22:58 +0900

    라이브러리 업데이트

--- ManalithBotWeb/project/Build.scala
+++ ManalithBotWeb/project/Build.scala
@@ -14,7 +14,7 @@ object ApplicationBuild extends Build {
 		javaJpa,
 		"org.hibernate" % "hibernate-entitymanager" % "3.6.10.Final",
 		"org.hsqldb" % "hsqldb" % "2.3.1",
-		"org.apache.commons" % "commons-lang3" % "3.1")
+		"org.apache.commons" % "commons-lang3" % "3.2.1")
 
 	val main = play.Project(appName, appVersion, appDependencies).settings(
 		// Add your own project settings here
