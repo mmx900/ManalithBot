899473109c9e29f415cf9399498de2722eafa0f6
mmx900 <mmx900@gmail.com>
2014-02-02 18:26:04 +0900

    [#141] 웹사이트를 Scala + Slick + AngularJS 기반으로 변경

---
+++ ManalithBotWeb/public/js/main.js
@@ -0,0 +1,7 @@
+var manalithApp = angular.module('manalithBotWeb', [ 'ui.bootstrap' ]);
+
+function MainCtrl($scope, $http, $document, $modal, orderByFilter) {
+	$scope.showDictionaryList = function() {
+
+	}
+}
